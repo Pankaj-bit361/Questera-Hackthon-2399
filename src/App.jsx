5e2f2a970de2191eff145e98a4f30e18abcbdcc1
pankaj <pankaj.v@questlabs.biz>
2025-12-09 20:14:40 +0530

    change to hashrouter

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import LoginPage from './components/LoginPage';
 import HomePage from './components/HomePage';
 import ChatPage from './components/ChatPage';
